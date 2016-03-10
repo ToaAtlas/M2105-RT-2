f39ab2acfd2648f797a7cb030a447f135b3aaa3d
ToaAtlas <21505645@etu.unicaen.fr>
2016-03-10 18:02:20 +0100

    Synchronisation des fichiers du TP1

--- ex3.php
+++ ex3.php
@@ -1,2 +1,16 @@
-<?php function createHTMLTable
+<?php function createHTMLTable($nbLignes,$nbCols)
+{
+    echo "<table border='1'>";
+    for ($i = 0; $i < $nbLignes; $i++) {
+        echo "<tr>";
+        for ($j = 0; $j < $nbCols; $j++) {
+            echo "<td>{$i}-{$j}</td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+}
+
+echo "Le tableau de l''exerciçe 3  est : ".createHTMLTable(2,5)
 ?>
+
